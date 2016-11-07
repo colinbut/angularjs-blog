cb4d7f33ef7d67423d4e35d9b246bf77bccbbe7d
Colin But <colinbut@users.noreply.github.com>
2016-11-07 19:29:15 +0000

    setting some mock data to work with

--- js/controllers.js
+++ js/controllers.js
@@ -4,5 +4,20 @@ var blogControllers = angular.module('blogControllers', []);
 
 blogControllers.controller('BlogController', ['$scope',
   function BlogController($scope) {
-      $scope.blogArticle = "This is a blog post about AngularJS. We will cover how to build a blog and how to add comments to a blog post";
+      // setting some mock data
+      $scope.blogList = [
+        {
+          "_id": 1,
+          "date": 1400623623107,
+          "introText": "This is a blog post about AngularJS.",
+          "blogText": "We will cover how to build a blog and how to add comments to the blog post"
+        },
+        {
+          "_id": 2,
+          "date": 1400667723107,
+          "introText": "In this blog post we will learn how to build applications based on REST",
+          "blogText": "Showing how to build REST web services that contain most of the business logic needed for applications"
+        }
+      ];
+
   }]);
