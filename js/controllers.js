d07dc20e001b3db7b729450717a5eb61173d9409
Colin But <colinbut@users.noreply.github.com>
2016-11-12 16:19:54 +0000

    Removed mock data with real data from REST call

--- js/controllers.js
+++ js/controllers.js
@@ -24,45 +24,18 @@ blogControllers.controller('BlogController', ['$scope',
   }]);
 
 // a Controller for managing the display of specific/particular blog posts
-blogControllers.controller('BlogViewController', ['$scope', '$routeParams',
-  function BlogViewController($scope, $routeParams) {
+blogControllers.controller('BlogViewController', ['$scope', '$routeParams','BlogPost'
+  function BlogViewController($scope, $routeParams, BlogPost) {
     var blogId = $routeParams.id;
-    var blog1 = {
-      "_id1": 1,
-      "date":1400623623107,
-      "introText": "This is a blog post about AngularJS",
-      "blogText": "We will cover how to build a blog and how to add comments to blog posts",
-      "comments" : [
-        {
-          "commentText": "Very goof post. I love it"
-        },
-        {
-          "commentText": "I would like to see a post comparing with React"
-        }
-      ]
-    };
-    var blog2 = {
-      "_id1": 2,
-      "date":1400623623107,
-      "introText": "How to write up a backend for AngularJS",
-      "blogText": "We will show how to write a backend RESTful web service(s) for AngularJS frontend",
-      "comments" : [
-        {
-          "commentText": "Nice, i like it"
-        },
-        {
-          "commentText": "Does the backend REST web services have to be in Java"
-        },
-        {
-          "commentText": "Can we use NodeJS for the backend REST?"
-        }
-      ]
-    };
 
-    if (blogId === '1') {
-      $scope.blogEntry = blog1;
-    } else if (blogId === '2') {
-      $scope.blogEntry = blog2;
-    }
-
+    BlogPost.get({id: blogId},
+      function success(response) {
+        console.log("Success:" + JSON.stringify(response));
+        $scope.blogEntry = response;
+      },
+      function error(errorResponse) {
+        console.log("Error:" + JSON.stringify(errorResponse));
+      }
+    );
+    
   }]);
