b4be28b48e12c881d1fc3635c37016a030747ee9
Colin But <colinbut@users.noreply.github.com>
2016-11-13 17:38:26 +0000

    added logout feature

--- js/app.js
+++ js/app.js
@@ -18,6 +18,14 @@ blogApp.config(['$routeProvider', '$locationProvider',
     .when('/blogPost/:id', {
       templateUrl: 'partials/blogPost.html',
       controller: 'BlogViewController'
+    })
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'LoginController'
+    })
+    .when('/logout', {
+      templateUrl: 'partials/login.html',
+      controller: 'LogoutController'
     });
 
     $locationProvider.html5Mode(false).hashPrefix('!');
