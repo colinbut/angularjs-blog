726af35932e4c9209e85cb70b92501342b24eb7b
Colin But <colinbut@users.noreply.github.com>
2016-11-14 21:02:39 +0000

    new REST backend service for blogposts

--- js/services.js
+++ js/services.js
@@ -17,10 +17,17 @@ blogServices.factory('BlogList', ['$resource',
 
 blogServices.factory('BlogPost', ['$resource',
   function($resource) {
-    return $resource("http://localhost/rs/blogPost", {} {
+    return $resource("http://localhost/rs/blogPost", {}, {
       get: {method: 'GET', cache: false, isArray: false},
       save: {method: 'POST', cache: false, isArray: false},
       update: {method: 'PUT', cache: false, isArray: false},
       delete: {method: 'DELETE', cache: false, isArray: false},
+    });
+  }]);
+
+blogServices.factory('BlogPostComments', ['$resource',
+  function($resource) {
+    return $resource("http://localhost/rs/blogPost/comment/:id", {}, {
+      save: {method: 'POST', cache: false, isArray: false}
     });
   }]);
